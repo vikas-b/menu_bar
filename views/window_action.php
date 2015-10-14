5a1ba54038806f403e908365c81b11e3d5c6c911
cis <cis@CISM316.(none)>
2015-10-14 16:37:34 +0530

    change the menu background colour

--- views/window_action.php
+++ views/window_action.php
@@ -4,24 +4,10 @@
     {
 	$file_to_search  	= isset($_POST['folder_name']) ? $_POST['folder_name'] : '';
 	
-	$files = array();
-	$root_path = SEARCH_PATH; 
-	
-	foreach ($iterator = new RecursiveIteratorIterator(
-	    new RecursiveDirectoryIterator($root_path, 
-		RecursiveDirectoryIterator::SKIP_DOTS),
-	    RecursiveIteratorIterator::SELF_FIRST) as $value) {
-	    
-	    if($value->getFilename() == $file_to_search) {
-		$files[] = $value->getPathname();
-		if(!empty($files))
-		{
-		    foreach($files as $value)
-		    {
-		    echo "$value<br/>";
-		    } 
-		}
-	    }
+	print '<pre>';
+	$files = glob('/opt/lampp/htdocs/'.$file_to_search.'/{,.}*', GLOB_BRACE);
+	foreach ($files as $filename) {
+	    echo "$filename " . "\n"; 
 	}
     }
 ?>
